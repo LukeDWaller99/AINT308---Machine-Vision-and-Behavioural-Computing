994ac1d818ab5f6e6637221d4150ca37f2fea0de
LukeDWaller99 <lukedwaller99@gmail.com>
2022-03-01 19:53:03 +0000

    First Draft of write for section 2

--- CW1/Task2/Task2/main.cpp
+++ CW1/Task2/Task2/main.cpp
@@ -38,7 +38,9 @@ int main()
 
         //load next frame
 
+        // points used to draw the vertical line for the origin of the pendulum
         Point point_1(592, 52), point_2(592, 650);
+
         int thickness = 2;
         int radiusOfCircle = 20;
 
@@ -48,30 +50,46 @@ int main()
              Scalar(0, 0, 255), // colour of line
              thickness); //line thickness
 
+        // used to convert the frame to the HSV colour space
         cvtColor(Frame, FrameHSV, COLOR_BGR2HSV);
+
+        // vectors for the upper and lower limits of the green for the card
+        // this is the colour that the frame will be tracking
         Vec3b greenLowerBound(30, 50, 70);
         Vec3b greenUpperBound(95, 255, 255);
 
         inRange(FrameHSV, greenLowerBound, greenUpperBound, FrameFiltered);
         Moments moment = moments(FrameFiltered, true);
 
-        Point pendulumPoint(moment.m10/moment.m00, //
-                moment.m01/moment.m00); //
+        // point in the centre of the rectangle found
+        Point pendulumPoint(moment.m10/moment.m00,
+                moment.m01/moment.m00);
 
+        // draws the circle of the centre of the rectangle
         circle(Frame, // target frame
                pendulumPoint, // center of circle
                radiusOfCircle, //radius of circle
                Scalar(0,255,0), // colour of circle
                thickness * 1.5); //thickness of line
+
+        // draws the line corrisponding to the pendulum
         line(Frame, // target frame
              Pivot, // starting point of line
              pendulumPoint, // ending point for line
              Scalar(255,0,0), //colour of line
              thickness); //thickness of line
 
+        // calulates the angle of the pendulum in radians
         double angleInRadians = (atan2(pendulumPoint.x, pendulumPoint.y )/ M_PI_4) - M_PI_4;
         char angleInRadiansOutput[30];
         sprintf(angleInRadiansOutput, "Angle in radians: %f", angleInRadians);
+
+        // calcualtes the angle of the pendulum in degress
+        double angleInDegrees = angleInRadians * 180 / CV_PI;
+        char angleInDegreesOutput[30];
+        sprintf(angleInDegreesOutput, "Angle in degrees: %f", angleInDegrees);
+
+        // outputting the angle in radians onto the screen
         putText(Frame, //target image
                 angleInRadiansOutput, // text to be outputted
                 Point(800, 30), // top-left position of text
@@ -80,10 +98,7 @@ int main()
                 Scalar(0, 255, 0), // text colour
                 thickness);
 
-        double angleInDegrees = angleInRadians * 180 / CV_PI;
-        char angleInDegreesOutput[30];
-        sprintf(angleInDegreesOutput, "Angle in degrees: %f", angleInDegrees);
-
+        // outputting the angle in degrees onto the screen
         putText(Frame, //target image
                 angleInDegreesOutput, //text
                 Point(800, 60), // top-left position of text
@@ -92,9 +107,10 @@ int main()
               Scalar(255, 0, 0), // text colour
                 thickness);
 
-
+        // prints the angle of the pendulum in degrees to the terminal
         cout << "Pendulum angle:  " << angleInDegrees << endl;
 
+        // writes the angles in degrees to a CSV file to be plotted
         DataFile << angleInDegrees << endl;
 
         //if frame is empty then the video has ended, so break the loop
